2815592d1230f9f1b1e0ba5504d87f23659888ff
everlasting17th <everlasting17th@gmail.com>
2024-07-03 20:07:06 +0400

    Add components lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { PrismaneProvider, TextField } from '@prismane/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PrismaneProvider>
+      <TextField placeholder='enter text' />
+    </PrismaneProvider>
   );
 }
 
